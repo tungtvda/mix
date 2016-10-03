4a70ce72db6bdf5e8d3b714a04075efbabecf7ec
tungtv <tungtv@qsoft.com.vn>
2016-10-03 14:02:23 +0700

    Tungtv2 update code

--- view/default/danhmuctour.php
+++ view/default/danhmuctour.php
@@ -11,6 +11,16 @@ function show_danhmuctour($data = array())
 {
     $asign = array();
 
+    $asign['danhsach'] = "";
+    if(count($data['danhsach'])>0)
+    {
+        $asign['danhsach'] = print_item('danhmuc', $data['danhsach']);
+    }
+    $asign['danhsach2'] = "";
+    if(count($data['danhsach'])>0)
+    {
+        $asign['danhsach2'] = print_item('packages', $data['danhsach']);
+    }
 
 
     print_template($asign, 'danhmuctour');
